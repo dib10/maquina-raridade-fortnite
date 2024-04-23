f9fdbc6e9f5677f8b99d59a467c7ad747e63208d
dib10 <caiodib10@gmail.com>
2024-04-23 14:45:01 -0300

    tratando raridade e colorindo com escapo Ansi

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-armas = ["Espingarda guardião do portão","SMG Arauto", "Rifle A.E caçada lunar","Rifle de Assalto Belicoforjado","Escopeta Martelo","Espingarda Automática Frenética","RA Nêmesis", "SMG Rajada de Trovão","Rifle de Precisão do Ceifador, Pistola Sentinela,","Canhão de Mão"]
+import random
 
-print("Bem vindo à Máquina de itens aleatórios do fortnuite")
+
+armas = {
+    "Espingarda guardião do portão": ["comum","incomum","rara","épica","lendária"],
+    "SMG Arauto": ["comum","incomum","rara","épica","lendária"],
+    "Rifle A.E caçada lunar": ["comum","incomum","rara","épica","lendária"], 
+    "Rifle de Assalto Belicoforjado": ["comum","incomum","rara","épica","lendária"], 
+    "Escopeta Martelo": ["comum","incomum","rara","épica","lendária"],
+    "Espingarda Automática Frenética": ["comum","incomum","rara","épica","lendária"], 
+    "RA Nêmesis": ["comum","incomum","rara","épica","lendária"],
+    "SMG Rajada de Trovão": ["comum","incomum","rara","épica","lendária"],
+    "Rifle de Precisão do Ceifador": ["comum","incomum","rara","épica","lendária"],
+    "Pistola Sentinela": ["comum","incomum","rara","épica","lendária"],
+    "Canhão de Mão": ["comum","incomum","rara","épica","lendária"]
+}
+
+print("Bem vindo à Máquina de itens aleatórios do fortnite")
+
+def maquina_sortida():
+    item = random.choice(list(armas.keys()))
+    raridade = random.choice(armas[item])
+    raridade_colorida = tratar_cores_raridade(raridade)
+
+    print(f"Você recebeu a arma {item} de raridade {raridade_colorida}")
+
+def tratar_cores_raridade(raridade):
+    if raridade == "comum":
+        return f"\033[90m{raridade}\033[0m"  # Cinza
+    elif raridade == "incomum":
+        return f"\033[32m{raridade}\033[0m"  # Verde
+    elif raridade == "rara":
+        return f"\033[34m{raridade}\033[0m"  # Azul
+    elif raridade == "épica":
+        return f"\033[35m{raridade}\033[0m"  # Roxo
+    elif raridade == "lendária":
+        return f"\033[33m{raridade}\033[0m"  # Amarelo
+    
+
+maquina_sortida()
