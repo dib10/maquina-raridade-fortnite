6f0edf1dfc15b2c032dbb43f637c37747515f1c5
dib10 <caiodib10@gmail.com>
2024-04-23 14:02:59 -0300

    iniciando repositorio

---
+++ main.py
@@ -0,0 +1,3 @@
+armas = ["Espingarda guardião do portão","SMG Arauto", "Rifle A.E caçada lunar","Rifle de Assalto Belicoforjado","Escopeta Martelo","Espingarda Automática Frenética","RA Nêmesis", "SMG Rajada de Trovão","Rifle de Precisão do Ceifador, Pistola Sentinela,","Canhão de Mão"]
+
+print("Bem vindo à Máquina de itens aleatórios do fortnuite")
